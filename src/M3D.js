3d9184754815719a68378c7a836b7fa9fa0649ab
shrekshrek@gmail.com <shrekshrek@gmail.com>
2017-11-09 17:06:20 +0800

    update

---
+++ src/M3D.js
@@ -0,0 +1,19 @@
+
+export { EventDispatcher } from './core/EventDispatcher.js';
+export { Object3D } from './core/Object3D.js';
+export { Scene } from './core/Scene.js';
+export { Camera } from './core/Camera.js';
+export { Group } from './core/Group.js';
+
+export { _Math as Math } from './math/Math.js';
+export { Euler } from './math/Euler.js';
+export { Matrix3 } from './math/Matrix3.js';
+export { Matrix4 } from './math/Matrix4.js';
+export { Quaternion } from './math/Quaternion.js';
+export { Vector3 } from './math/Vector3.js';
+
+export { CssSprite } from './renderer/CssSprite.js';
+export { CssObject } from './renderer/CssObject.js';
+export { CssRenderer } from './renderer/CssRenderer.js';
+
+
