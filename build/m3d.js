369e2abdfb53d2a53ad4b760154f2604b3b1fa65
shrekshrek@gmail.com <shrekshrek@gmail.com>
2017-12-07 13:02:10 +0800

    update

--- build/m3d.js
+++ build/m3d.js
@@ -1,3 +1,8 @@
+/*!
+ * GIT:https://github.com/shrekshrek/css3d-matrix-es6
+ * @author: Shrek.wang
+ **/
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
-export default {
+import uglify from 'rollup-plugin-uglify';
+
+export default [{
 	entry: 'src/M3D.js',
 	indent: '\t',
 	targets: [
@@ -7,5 +9,19 @@ export default {
 			moduleName: 'M3D',
 			dest: 'build/m3d.js'
 		}
-	]
-};
+	],
+    banner: '/*!\n * GIT:https://github.com/shrekshrek/css3d-matrix-es6\n * @author: Shrek.wang\n **/\n',
+},{
+    entry: 'src/M3D.js',
+    indent: '\t',
+    targets: [
+        {
+            format: 'umd',
+            moduleName: 'M3D',
+            dest: 'build/m3d.min.js'
+        }
+    ],
+    plugins: [
+        uglify()
+    ],
+}];
